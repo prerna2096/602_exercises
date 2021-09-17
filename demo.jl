6a9181e8ac84af729ff582c33a439d6a820ecc72
prerna2096 <prerna96.kundu@gmail.com>
2021-09-17 09:36:45 -0700

    commit on 'main'

--- demo.jl
+++ demo.jl
@@ -1,6 +1,26 @@
+#### Tutorial 2 ###
+
 my_integer = 5
 my_float = 5
 my_range = 1:7
+my_vector = [1,3,5] 
 my_string = "hello"
 my_character = 'o'
 my_boolean = true
+
+typeof(my_integer)
+typeof(my_float)
+
+typeof( 
+    my_character
+)
+α = 5;
+@show α + my_integer
+print("α = $α\n", "My string is: $(my_string)")
+
+
+#####****** Accessing packages 
+#* To enter the package manager: "]" in REPL
+#* Current environment shown in parenthesis
+#* Type activate . after (bin) pkg to create a new environment 
+#- want to have a new environment when working on new projects
